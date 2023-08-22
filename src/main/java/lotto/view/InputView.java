2310ee790ffbedd345dae7e131fb67a5073f9041
yuseonjun <seonjun0906@gmail.com>
2023-08-22 17:41:25 +0900

    feat: 당첨번호 생성을 위한 당첨번호 입력, 보너스 번호 입력 기능 구현

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -11,4 +11,16 @@ public class InputView {
         final String price = Console.readLine();
         return InputValidator.validatePrice(price);
     }
+
+    public List<Integer> inputPrizeLottoNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String prizeLottoNumber = Console.readLine();
+        return InputValidator.validatePrizeLottoNumbers(prizeLottoNumber);
+    }
+
+    public int inputBonusLottoNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        final String bonusLottoNumber = Console.readLine();
+        return InputValidator.validateBonusLottoNumber(bonusLottoNumber);
+    }
 }
