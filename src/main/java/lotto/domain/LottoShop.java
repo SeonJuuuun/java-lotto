68644f2792b52b3cc77360a94d52977b9b6574bc
yuseonjun <seonjun0906@gmail.com>
2023-08-22 17:44:15 +0900

    feat: 구입 금액, 당첨번호에 대한 검증 기능 구현

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -29,3 +29,35 @@ public class LottoShop {
         validCheckPrizeLottoNumber(inputPrizeLottoNumber);
         return new Lotto(inputPrizeLottoNumber);
     }
+
+    private void validCheckPrice(int price) {
+        checkPriceRange(price);
+        checkMoney(price);
+    }
+
+    private void checkMoney(int price) {
+        if (price % ONE_LOTTO_PRICE != 0) {
+            throw new IllegalArgumentException(
+                LottoConstants.ERROR_MESSAGE + "1000원 단위로 입력을 해야합니다.");
+        }
+    }
+
+    private void checkPriceRange(int price) {
+        if (price < ONE_LOTTO_PRICE) {
+            throw new IllegalArgumentException(LottoConstants.ERROR_MESSAGE + "구입은 1000원부터 가능합니다.");
+        }
+    }
+
+    private void validCheckPrizeLottoNumber(List<Integer> inputPrizeLottoNumber) {
+        checkPrizeLottoRange(inputPrizeLottoNumber);
+    }
+
+    private void checkPrizeLottoRange(List<Integer> inputPrizeLottoNumber) {
+        for (int lottoNumber : inputPrizeLottoNumber) {
+            if (lottoNumber < 1 || lottoNumber > 45) {
+                throw new IllegalArgumentException(
+                    LottoConstants.ERROR_MESSAGE + "당첨 번호 입력은 1이상 45이하의 숫자만 입력 가능합니다.");
+            }
+        }
+    }
+}
