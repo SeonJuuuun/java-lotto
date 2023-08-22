e6a79f3c92873e9fc4784945b8dca1ebaf4b6d71
yuseonjun <seonjun0906@gmail.com>
2023-08-22 17:48:56 +0900

    feat: Controller를 총괄하는 Application 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,21 @@
 package lotto;
 
+import lotto.controller.Controller;
+import lotto.domain.LottoShop;
+import lotto.domain.RandomNumberGenerator;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Controller controller = new Controller(new InputView(),
+            new LottoShop(new RandomNumberGenerator()), new OutputView());
+        try {
+            controller.play();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
